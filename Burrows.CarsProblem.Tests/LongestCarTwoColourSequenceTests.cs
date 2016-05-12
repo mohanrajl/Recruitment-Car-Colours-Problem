c9bf06c32bf977b707da83121c50455056c38f4a
Alan Evans <alan.evans@burrows.yr.com>
2016-05-12 12:38:57 +0100

    Cars Problem

---
+++ Burrows.CarsProblem.Tests/LongestCarTwoColourSequenceTests.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Burrows.CarsProblem.Tests
+{
+    [TestFixture]
+    public class LongestCarTwoColourSequenceTests
+    {
+        [Test]
+        public void when_no_cars()
+        {
+            Assert.AreEqual(0, CalculateLongestSequenceOfTwoCarColours(new string[0]));
+        }
+
+        [Test]
+        public void when_one_car()
+        {
+            Assert.AreEqual(1, CalculateLongestSequenceOfTwoCarColours(new[] {"Red"}));
+        }
+
+        [Test]
+        public void when_two_identical_cars()
+        {
+            Assert.AreEqual(2, CalculateLongestSequenceOfTwoCarColours(new[] {"Red", "Red"}));
+        }
+
+        [Test]
+        public void when_two_different_cars()
+        {
+            Assert.AreEqual(2, CalculateLongestSequenceOfTwoCarColours(new[] {"Red", "Blue"}));
+        }
+
+        // Your first failing test has been written for you here
+        [Test]
+        public void when_three_different_cars()
+        {
+            Assert.AreEqual(2, CalculateLongestSequenceOfTwoCarColours(new[] {"Red", "Blue", "Green"}));
+        }
+
+        // add more tests, feel free to refactor all the tests as you see fit
+        
+        /// <summary>
+        /// Given a sequence of car colours, returns the length of longest streak containing just two colours.
+        /// See read me for more information and examples.
+        /// Complete this method however you wish.
+        /// You may call other functions and classes, but please do not change this methods signature.
+        /// </summary>
+        /// <param name="carColours"></param>
+        /// <returns></returns>
+        public static int CalculateLongestSequenceOfTwoCarColours(IEnumerable<string> carColours)
+        {
+            return carColours.Count();
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,20 @@
+Car Colour Sequence Problem
+===
+
+The busy Burrows dev team is observing cars passing the office window. They notice that there is occasionally a rather long sequence of cars of only *two* colours.
+
+One time they observe `red, green, red, red, green`, a sequence of 5 cars sharing just two colours before a `blue` car comes along and ends the run.
+
+![CarSequence1.png](CarSequence1.png)
+
+Another time they observe `blue, yellow, black, yellow, green, yellow, yellow, red` and identify the two-colour sequence `yellow, green, yellow, yellow`as being longer than any of the other sequences.
+
+![CarSequence2.png](CarSequence2.png)
+
+The team decides to commission *you* to write a piece of software to find the longest sequence of two car colours so that they can all get on with something more productive.
+
+Using a TDD aproach, complete the method in this repo:
+
+    public static int CalculateLongestSequenceOfTwoCarColours(IEnumerable<string> carColours)
+
+> Once you're done, please do not submit a pull request, as we want to keep your solution private, please zip and email the whole repo when you're done. And if you have Git experience, please do branch as you normally would and provide a commit history.
